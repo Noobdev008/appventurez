1f76c7b42cca4582bd147085086744a3b6b22782
Shubham Srivastava <86356397+Noobdev008@users.noreply.github.com>
2022-06-03 17:45:20 +0530

    Node js installation and use of REPL

---
+++ Appventurez/Node_tut/index.js
@@ -0,0 +1,69 @@
+// what is REPL in Node.JS. So, REPL stands for Read Eval Print Loop and
+//  it represents a computer environment like a Windows console or Unix/Linux shell where a command
+// is entered and the system responds with output in an interactive mode. Node.js or
+//  Node comes bundled with a REPL environment. It performs the following tasks −
+
+// Read − Reads the user's input, parses the input into JavaScript data-structure, and stores in memory.
+
+// Eval − Takes and evaluates the data structure.
+
+// Print − Prints the result.
+
+// Loop − Loops the above command until the user presses ctrl-c twice.
+
+// The REPL feature of Node is very useful in experimenting with Node.js codes and to debug JavaScript codes.
+
+
+
+// REPL
+ //1. js expression
+ //2. use variable
+ //3. multiline code/loops
+ //4. use (_) to get the last result 
+ //5. use editor mode 
+
+
+ //1. js expression- 
+
+ // So first of install node in your laptop after installation in vs code go to terminal and write node
+ // you can reach the node 
+ // so with the help of js expression we can use opreatores like - plus , minus , divide etc..
+//  eg: - 3+3 , 4-3 ... etc
+
+ //2. use variable-
+ // you can use variables like - 
+ // a= 3 , b= 4 
+ // a+b => 7
+
+ //3. multiline code/loops
+
+// we can also use multiline code or loops in node js terminal
+// eg : 
+var x =0
+do{
+  x++;
+console.log(x)
+}while(x<5) // 1,2,3,4,5
+
+
+
+ //4. use (_) to get the last result 
+
+ // you can get your previous output very easily
+ // eg:
+ // 5+5
+ // 10
+ // 10+5
+ //15   //now suppose you forget your last result so in the terminal you use _ and you get last result vlue
+// _
+// 15   //so if you add like  - _ + 5 so result will 20 
+// _ + 5
+//20
+
+
+ //5. use editor mode 
+
+ // in the editor mode you can write your programm very normally like vs code editor etc .
+ // It works same 
+ // After writing the code press ctrl+D to finsh the task and for exiting ctrl+C to exit from node js terminal.
+
