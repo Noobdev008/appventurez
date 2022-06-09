bec3c6b9050c228a4815f1c880887c073c447907
Shubham Srivastava <shubhamsri0807@gmail.com>
2022-06-09 15:35:57 +0530

    commitiing in main

---
+++ Appventurez/7/7.2_property-accessors.js
@@ -0,0 +1,13 @@
+// Getters and setters
+// Accessor properties are represented by “getter” and “setter” methods. In an object literal they are denoted by get and set:
+
+// let obj = {
+//   get propName() {
+//     // getter, the code executed on getting obj.propName
+//   },
+
+//   set propName(value) {
+//     // setter, the code executed on setting obj.propName = value
+//   }
+// };
+// The getter works when obj.propName is read, the setter – when it is assigned...
