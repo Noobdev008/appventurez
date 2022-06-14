425c58132fcb54a1c06d8b7a991ae7910f510694
Shubham Srivastava <shubhamsri0807@gmail.com>
2022-06-14 16:19:54 +0530

    updated code

--- Appventurez/11/11.4_Promise-Error-Handling.js
+++ Appventurez/11/11.4_Promise-Error-Handling.js
@@ -5,7 +5,9 @@ new Promise(function(resolve, reject) {
   setTimeout(() => {
     throw new Error("Whoops!");
   }, 1000);
-}).catch(alert);
+}).catch((err)=>{
+    console.log(err);
+});
 
 // Answer is "No";
 //Why? 
