0f517e857fd72619cf9ad967a00b150313d1b673
Shubham Srivastava <shubhamsri0807@gmail.com>
2022-06-14 15:36:21 +0530

    Promise-Error

---
+++ Appventurez/11/11.4_Promise-Error-Handling.js
@@ -0,0 +1,15 @@
+// Error in setTimeout
+// What do you think? Will the .catch trigger? Explain your answer.
+
+new Promise(function(resolve, reject) {
+  setTimeout(() => {
+    throw new Error("Whoops!");
+  }, 1000);
+}).catch(alert);
+
+// Answer is "No";
+//Why? 
+
+// an "implicit try..catch" around the function code. So all synchronous errors are handled.
+// But here the error is generated not while the executor is running, but later. 
+// So the promise can’t handle it.
