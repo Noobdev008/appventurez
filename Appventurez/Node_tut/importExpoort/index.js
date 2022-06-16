0476b99cd27d662ccc92f88c1260a95a519324e5
Shubham Srivastava <shubhamsri0807@gmail.com>
2022-06-16 18:23:52 +0530

    node tut

---
+++ Appventurez/Node_tut/importExpoort/index.js
@@ -0,0 +1,6 @@
+const {add,sub,div,mul,name} = require('./opex')
+console.log(add(5,5)); //10
+console.log(sub(10,5)); //5
+console.log(div(20,5));//4
+console.log(mul(3,5));//15
+console.log(name)///shubham
