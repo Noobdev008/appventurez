a94adacf26d5e1b57463f45f4b6220c33dcb02fc
Shubham Srivastava <shubhamsri0807@gmail.com>
2022-06-20 11:40:33 +0530

    nodemon install

---
+++ Appventurez/Node_tut/importExpoort/opex.js
@@ -0,0 +1,20 @@
+
+const add =(a,b)=>{
+    return a+b
+};
+const sub =(a,b)=>{
+    return a+b
+};
+const div =(a,b)=>{
+    return a/b
+};
+const mul =(a,b)=>{
+    return a*b
+};
+
+
+
+const name= 'shubham'
+// module.exports = add; //for one module
+
+module.exports ={add ,sub ,div ,mul,name} //for two modules
