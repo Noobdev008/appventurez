5bda717cec105bbd3e1688f7046eac7ff1326ac8
Shubham Srivastava <shubhamsri0807@gmail.com>
2022-06-20 12:50:52 +0530

    converting to json and vice-versa

---
+++ Appventurez/Node_tut/JSON/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+
+const bioData = {
+    name: 'shubham', 
+    age : 24,
+    designation: 'Software Engineer',
+};
+
+
+const jsonData = JSON.stringify(bioData); // convert to json
+
+// fs.writeFile('json1.json',jsonData, (err)=>{
+//  console.log("done");
+// })
+
+
+fs.readFile('json1.json','utf-8',(err,data)=>{
+//   console.log(data);
+const orgData = JSON.parse(data); // convert to OBJ
+console.log(orgData);
+})
