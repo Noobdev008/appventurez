94c62c0c7139202e936402a69a1ea6c1cd6b85a2
Shubham Srivastava <shubhamsri0807@gmail.com>
2022-06-20 12:49:16 +0530

    converting json to obj and vice cersa

---
+++ Appventurez/Node_tut/JSON/JSON.js
@@ -0,0 +1,28 @@
+// // JSON stands for JavaScript Object Notation
+
+// // JSON is a text format for storing and transporting data
+
+// // JSON is "self-describing" and easy to understand
+
+// What is JSON?
+// JSON stands for JavaScript Object Notation
+// JSON is a lightweight data-interchange format
+// JSON is plain text written in JavaScript object notation
+// JSON is used to send data between computers
+// JSON is language independent *
+
+const bioData = {
+    name: 'shubham', 
+    age : 24,
+    designation: 'Software Engineer',
+}
+
+const jsonData = JSON.stringify(bioData); // convert to JSON
+console.log(jsonData); //{"name":"shubham","age":24,"designation":"Software Engineer"}
+
+console.log(jsonData.designation); // undefined;
+
+const objData = JSON.parse(jsonData); // convert to Object
+console.log(objData); //{ name: 'shubham', age: 24, designation: 'Software Engineer' }
+
+console.log(objData.designation); // Software Engineer;
