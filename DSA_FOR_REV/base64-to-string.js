c4b0b1180bbdcced3fde8cf9b02e44a682827081
Shubham Srivastava <86356397+Noobdev008@users.noreply.github.com>
2022-07-07 18:44:46 +0530

    Add files via upload

---
+++ DSA_FOR_REV/base64-to-string.js
@@ -0,0 +1,5 @@
+let secretKey =  "WW91IGFyZSBmb29s"
+
+let decodedSecretKey =  atob(secretKey );
+
+console.log(decodedSecretKey + " Lol!");
---
+++ DSA_FOR_REV/string-to-base64.js
@@ -0,0 +1,6 @@
+// let string =  'I am sending a secret message to you';
+let string = 'You are fool';
+
+let encoded = btoa(string);
+console.log(encoded);
+// SSBhbSBzZW5kaW5nIGEgc2VjcmV0IG1lc3NhZ2UgdG8geW91
