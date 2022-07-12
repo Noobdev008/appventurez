3bab4a7258e422bed95160c839db38444c4eda7d
Shubham Srivastava <86356397+Noobdev008@users.noreply.github.com>
2022-07-12 13:19:56 +0530

    Add files via upload

--- Practice/findIndex.js
+++ Practice/findIndex.js
@@ -1,10 +1,18 @@
 let arr = [[1,2],[4,5],[3,4]]
+let search = [3,4]
 
+// for (let i in arr) {
+//     console.log(arr[i] +" " +i)
+// };
 
-for (let i in arr) {
-    console.log(arr[i] +" " +i)
+// arr.forEach((newArr,index)=>{
+//     console.log(newArr +" - "+index)
+// });
+
+const index = (array, search) => {
+    for(let i=0; i<array.length; i++){
+        if(JSON.stringify(array[i]) === JSON.stringify(search))   return i;
+    }
+    return -1;
 }
-
-arr.forEach((newArr,index)=>{
-    console.log(newArr +" - "+index)
-})
+console.log(index(arr,search))
