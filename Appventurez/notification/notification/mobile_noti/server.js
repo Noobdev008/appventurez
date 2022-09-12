1d3cc84cd6898148a72453b8545358d22cf97966
Shubham Srivastava <86356397+Noobdev008@users.noreply.github.com>
2022-09-12 12:38:47 +0530

    updated

--- Appventurez/notification/notification/mobile_noti/server.js
+++ Appventurez/notification/notification/mobile_noti/server.js
@@ -1,7 +1,9 @@
 var admin = require("firebase-admin");
-
+let db = require('./db/db.config');
 var serviceAccount = require('./notification-4d8cd-firebase-adminsdk-rzmns-97f56a2b5b.json');
 
+
+const Notifications = db.noti
 admin.initializeApp({
   	credential: admin.credential.cert(serviceAccount),
 	databaseURL: "https://prismappfcm.firebaseio.com"
@@ -10,16 +12,20 @@ var topic = 'general';
 
 var message = {
   notification: {
-    title: '',
-    body: ``
+    title: 'messages',
+    body: `xyz`
   },
   topic: topic
 };
+// console.log(message.notification.body);
+let updateMessage = message.notification.body
 
 // Send a message to devices subscribed to the provided topic.
 admin.messaging().send(message)
-  .then((response) => {
+  .then(async (response) => {
     // Response is a message ID string.
+    let data = await Notifications.create({message:updateMessage})
+    console.log(data , "data");
     console.log('Successfully sent message:', response);
   })
   .catch((error) => {
