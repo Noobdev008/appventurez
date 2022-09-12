6bc77905b878c0391489ff231bcdcd566eeefa14
Shubham Srivastava <86356397+Noobdev008@users.noreply.github.com>
2022-09-12 12:03:40 +0530

    Add files via upload

---
+++ Appventurez/notification/mobile_noti/server.js
@@ -0,0 +1,27 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require('./notification-4d8cd-firebase-adminsdk-rzmns-97f56a2b5b.json');
+
+admin.initializeApp({
+  	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "https://prismappfcm.firebaseio.com"
+});
+var topic = 'general';
+
+var message = {
+  notification: {
+    title: 'Message from Brazzers',
+    body: `Hey Sexy!`
+  },
+  topic: topic
+};
+
+// Send a message to devices subscribed to the provided topic.
+admin.messaging().send(message)
+  .then((response) => {
+    // Response is a message ID string.
+    console.log('Successfully sent message:', response);
+  })
+  .catch((error) => {
+    console.log('Error sending message:', error);
+});
